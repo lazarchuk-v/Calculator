e4faa1ce281b81478ef05a476715e9cd0c0a5cbd
marketing-specv <marketing.specv@gmail.com>
2022-12-22 20:47:46 +0200

    calc gyroscope

--- script/script.js
+++ script/script.js
@@ -37,4 +37,9 @@ VanillaTilt.init(document.querySelector(".calculator"), {
     speed: 400,
     glare: true,
     "max-glare": 0.2,
+    gyroscope:              true,   // Boolean to enable/disable device orientation detection,
+    gyroscopeMinAngleX:     -45,    // This is the bottom limit of the device angle on X axis, meaning that a device rotated at this angle would tilt the element as if the mouse was on the left border of the element;
+    gyroscopeMaxAngleX:     45,     // This is the top limit of the device angle on X axis, meaning that a device rotated at this angle would tilt the element as if the mouse was on the right border of the element;
+    gyroscopeMinAngleY:     -45,    // This is the bottom limit of the device angle on Y axis, meaning that a device rotated at this angle would tilt the element as if the mouse was on the top border of the element;
+    gyroscopeMaxAngleY:     45,     // This is the top limit of the device angle on Y axis, meaning that a device rotated at this angle would tilt the element as if the mouse was on the bottom border of the element;
 });
